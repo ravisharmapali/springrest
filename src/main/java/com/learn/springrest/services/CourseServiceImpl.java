1263e49947960f7a5f856e84698c9edd3c24b964
ravi <ravisharmapali@gmail.com>
2023-05-10 19:53:16 +0530

    added service implementation to get courses using id and add new course to dummy list

--- src/main/java/com/learn/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/learn/springrest/services/CourseServiceImpl.java
@@ -3,6 +3,7 @@ package com.learn.springrest.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.learn.springrest.entities.Course;
@@ -23,6 +24,30 @@ public class CourseServiceImpl implements ICourseService {
 	public List<Course> getCourses() {
 
 		return list;
+	}
+
+	@Override
+	public Course getCourse(long courseId) {
+
+		// finding course
+		Course c = null;
+		for (Course course : list) {
+			if (course.getCourseId() == courseId) {
+				c = course;
+				break;
+			}
+		}
+
+		return c;
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+
+		if (list.add(course))
+			return course;
+		else
+			return null;
 	}
 
 }
--- src/main/java/com/learn/springrest/services/ICourseService.java
+++ src/main/java/com/learn/springrest/services/ICourseService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import com.learn.springrest.entities.Course;
 
-
 public interface ICourseService {
-	
+
 	public List<Course> getCourses();
-	
+
+	public Course getCourse(long courseId);
+
+	public Course addCourse(Course course);
 }
